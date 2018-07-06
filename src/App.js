b602917e926fa817dabf10446e95e469313c2d96
altbdoor <lancersupraskyline@gmail.com>
2018-07-06 18:07:30 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+            <p>Lorem ipsum dolor sit amet.</p>
+        );
+    }
+}
+
+export default App;
